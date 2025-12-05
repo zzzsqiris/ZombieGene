4c23234510f645f2f30f48e9be3674d077bdc089
zzzsqiris <zhngshq826@gmail.com>
2025-12-05 00:56:15 -0800

    up

---
+++ getposition.py
@@ -0,0 +1,34 @@
+import argparse
+import gzip
+
+parser = argparse.ArgumentParser()
+parser.add_argument("infile")
+args = parser.parse_args()
+
+#parent = []
+start = 0
+end = 0
+d = {}
+with gzip.open(args.infile, 'rt') as f:
+    for line in f:
+        line = line.rstrip()
+        line = line.split('\t')
+        if line [2] != 'CDS' or line[6] != '+':
+            continue
+        
+        chr = line[0]   
+        cur_parent = line[8].split("=")[1]
+        start = int(line[3])
+        end = int(line[4])
+        if cur_parent not in d:
+            d[cur_parent] = [chr, []]
+        d[cur_parent][1].append((start, end))
+        
+
+    for parent in d:
+        chr, pos = d[parent]
+        print (parent, chr, pos, sep = '\t')
+
+        
+
+        
---
+++ pos2proteinseq.py
@@ -0,0 +1,65 @@
+import argparse
+import gzip
+import korflab
+
+parser = argparse.ArgumentParser()
+parser.add_argument("position_file")
+parser.add_argument("fa_file")
+args = parser.parse_args()
+
+
+# store chromosome seq into dictionary
+chr_dict = {}
+chr_num = ""
+
+with gzip.open(args.fa_file, 'rt') as f:
+    for line in f:
+        line = line.rstrip()
+        if line.startswith(">"):
+            if chr_num:
+                chr_dict[chr_num] = "".join(chr_seq)
+            chr_num = line[1:].split()[0]
+            chr_seq = []
+        else:
+            chr_seq.append(line)
+
+    if chr_num:
+        chr_dict[chr_num] = "".join(chr_seq)
+
+
+with open(args.position_file) as f:
+    for line in f:
+        line = line.rstrip()
+        parent, chr_num, pos_str = line.split("\t")
+        
+        # convert position string to int list
+        pos_str = pos_str.strip("[]")
+        pos_str = pos_str.split("), (")
+        pos = []
+        for pair in pos_str:
+            pair = pair.replace("(", "").replace(")", "")
+            start, end = pair.split(",")
+            pos.append((int(start), int(end)))
+
+        chr_seq = chr_dict[chr_num]
+        extron_seq = []
+        for start, end in pos:
+            extron_seq.append(chr_seq[start-1:end])
+
+        # translate
+        protein_seq = []
+        leftover = ""
+        for exon in extron_seq:
+            exon = leftover + exon
+            remainder = len(exon) % 3
+
+            if remainder == 0:
+                leftover = ""
+            else:
+                leftover = exon[-remainder:]
+                exon = exon[:-remainder]
+            
+            protein_seq.append(korflab.translate(exon))
+
+        linked_protein = "X".join(protein_seq)
+        print(parent, chr_num, linked_protein)
